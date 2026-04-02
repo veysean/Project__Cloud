c55bd88a1794b7150f5ba8c58086a522067d97e0
Chanalizain <inchanaliza@gmail.com>
2026-04-02 20:10:50 +0700

    implement multi-AZ public subnets for high availability

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -3,13 +3,33 @@ resource "aws_vpc" "main" {
   tags = { Name = "${var.project_name}-vpc" }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-southeast-1a"
-  tags = { Name = "${var.project_name}-public" }
+  tags = { Name = "${var.project_name}-public_a" }
 }
+
+resource "aws_route_table_association" "public_assoc_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24" 
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-southeast-1b" # Different AZ for High Availability
+  tags = { Name = "${var.project_name}-public-b" }
+}
+
+# ASSOCIATE SUBNET WITH THE INTERNET
+resource "aws_route_table_association" "public_assoc_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"  
@@ -37,7 +57,4 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public_rt.id
-}
+

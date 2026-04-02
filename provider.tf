d1c29cff55399f3f2d6234399825023865a1548c
Chanalizain <inchanaliza@gmail.com>
2026-04-02 22:20:54 +0700

    remove one provider block

--- provider.tf
+++ provider.tf
@@ -1,18 +1,14 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 terraform {
+  required_version = ">= 1.2"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.92"
     }
   }
-
-  required_version = ">= 1.2"
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = var.aws_region 
 }

f11014816bb27cab22fa723df1202b08188f9aba
Chanalizain <inchanaliza@gmail.com>
2026-04-02 21:04:15 +0700

    add clear note for each block of the vpc file

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -1,8 +1,11 @@
+# Defines the primary isolated network (VPC) for the project
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = { Name = "${var.project_name}-vpc" }
 }
 
+# Public subnet internet facing resources like the Load Balancer
+# We have them in two AZ for HA
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -11,25 +14,16 @@ resource "aws_subnet" "public_a" {
   tags = { Name = "${var.project_name}-public_a" }
 }
 
-resource "aws_route_table_association" "public_assoc_a" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24" 
   map_public_ip_on_launch = true
-  availability_zone       = "ap-southeast-1b" # Different AZ for High Availability
+  availability_zone       = "ap-southeast-1b"
   tags = { Name = "${var.project_name}-public-b" }
 }
 
-# ASSOCIATE SUBNET WITH THE INTERNET
-resource "aws_route_table_association" "public_assoc_b" {
-  subnet_id      = aws_subnet.public_b.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
+# Private subnet in AZ-a for internal resources like the RDS database
+# We have them in two AZ to provide redundancy for the DB layer
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"  
@@ -44,11 +38,13 @@ resource "aws_subnet" "private_b" {
   tags = { Name = "${var.project_name}-private-b" }
 }
 
+# IGW to allow communication between the VPC and the internet
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags   = { Name = "${var.project_name}-igw" }
 }
 
+# Route table containing the rule to direct outbound traffic to the internet
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
   route {
@@ -57,4 +53,13 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+# Associates public subnets with the internet routing rules
+resource "aws_route_table_association" "public_assoc_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
 
+resource "aws_route_table_association" "public_assoc_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_rt.id
+}
